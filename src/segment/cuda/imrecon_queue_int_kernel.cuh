6580fdf8068e5c4d3bef7d97591cf692d617411d
Tony Pan <tpan7@gatech.edu>
2011-09-08 10:58:35 -0400

    begin the gpu verison queue code

---
+++ src/segment/cuda/imrecon_queue_int_kernel.cuh
@@ -0,0 +1,13 @@
+/*
+ * kernel codes for the reconstruction by dilation (reconstruction_by_dilation_kernel)
+  */
+#include "internal_shared.hpp"
+
+namespace nscale { namespace gpu {
+
+template <typename T>
+unsigned int imreconstructIntCaller(T* g_marker, const T* g_mask, const int sx, const int sy,
+		const int connectivity, cudaStream_t stream );
+
+
+}}
