89070a345b14685d25379489fa7cbb3cbd2873f6
gteodor <gteodor@dhcp061124.psy.emory.edu>
2011-12-12 09:39:12 -0500

    Execution Engine modified to remove dependency from the features extraction.

---
+++ src/execEngine/ExecEngineConstants.h
@@ -0,0 +1,15 @@
+#ifndef EXEC_ENGINE_CONSTANTS_H_
+#define EXEC_ENGINE_CONSTANTS_H_
+
+using namespace std;
+
+class ExecEngineConstants {
+public:
+	//! Defining what processor should be used when invoking the functions
+	static const int NUM_PROC_TYPES=2;
+	static const int CPU=1;
+	static const int GPU=2;
+
+};
+
+#endif
