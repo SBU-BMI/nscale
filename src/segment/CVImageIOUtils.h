349935801c0b44455877aa582ec30b8f4572da65
Tony Pan <tcpan@emory.edu>
2011-12-29 15:27:35 -0500

    updated the intermediate results saver to not rely on #define.  this gives more flexibility during build.

--- src/segment/CVImageIOUtils.h
+++ src/segment/CVImageIOUtils.h
@@ -1,5 +1,7 @@
 /*
- * utils.h
+ * for outputting intermediate images.
+ * since the intermediate results may only be needed for certain executables, instead of relying on include to pickup the #defines,
+ * and risk that a library may not have been compiled with the right set, I am creating 2 versions of the class (so as to not to have branch).
  *
  *  Created on: Jul 15, 2011
  *      Author: tcpan
@@ -11,7 +13,6 @@
 #include "cv.h"
 #include <fstream>
 #include <iostream>
-#include <sys/time.h>
 
 namespace cciutils {
 
@@ -20,13 +21,48 @@ namespace cv {
 using ::cv::Exception;
 using ::cv::error;
 
-class IntermediateResultWriter {
+
+class IntermediateResultHandler {
+
+public:
+	virtual ~IntermediateResultHandler();
+
+	virtual void imwriteRaw(const ::cv::Mat& intermediate, const int stage) = 0;
+	virtual void saveIntermediate(const ::cv::Mat& intermediate, const int stage) = 0;
+
+	virtual void imwriteRaw(const ::cv::gpu::GpuMat& intermediate, const int stage) = 0;
+	virtual void saveIntermediate(const ::cv::gpu::GpuMat& intermediate, const int stage) = 0;
+};
+
+class IntermediateResultWriter : IntermediateResultHandler {
 private:
 	std::string prefix;
 	std::string suffix;
 	const std::string RAW;
 	std::vector<int> selectedStages;
 	::cv::gpu::Stream stream;
+
+	std::string getFileName(const int stage, const std::string &suf) {
+		stringstream ss;
+		ss << prefix << "-" << stage << "." << suf;
+		return ss.str();
+	};
+
+	bool selected(const int stage) {
+		std::vector<int>::iterator pos = std::lower_bound(selectedStages.begin(), selectedStages.end(), stage);
+		return (pos == selectedStages.end() || stage != *pos ) ? false : true;
+	};
+
+#if defined (HAVE_CUDA)
+	::cv::Mat download(const ::cv::gpu::GpuMat& intermediate) {
+		::cv::Mat output(intermediate.size(), intermediate.type());
+		stream.waitForCompletion();
+		stream.enqueueDownload(intermediate, output);
+		stream.waitForCompletion();
+		return output;
+	};
+#endif
+
 public:
 	IntermediateResultWriter(const std::string &pref, const std::string &suf, std::vector<int> selStages, ::cv::gpu::Stream &strm) {
 		prefix.assign(pref);
@@ -36,31 +72,14 @@ public:
 		stream = strm;
 	};
 
-	~IntermediateResultWriter() {
+	virtual ~IntermediateResultWriter() {
 		selectedStages.clear();
-	}
-
-	inline std::string getFileName(const int stage, const std::string &suf) {
-		stringstream ss;
-		ss << prefix << "-" << stage << "." << suf;
-		return ss.str();
-	}
-
-#define SAVE_INTERMEDIATES 1;
-
-
-#if !defined (SAVE_INTERMEDIATES)
-	// write out with raw
-	inline void imwriteRaw(const ::cv::Mat& intermediate, const int stage) {};
-	inline void saveIntermediate(const ::cv::Mat& intermediate, const int stage) {};
-
-	inline void imwriteRaw(const ::cv::gpu::GpuMat& intermediate, const int stage) {};
-	inline void saveIntermediate(const ::cv::gpu::GpuMat& intermediate, const int stage) {};
-
-#else
+	};
 
 	// write out with raw
-	inline void imwriteRaw(const ::cv::Mat& intermediate, const int stage) {
+	virtual void imwriteRaw(const ::cv::Mat& intermediate, const int stage) {
+		if (!selected(stage)) return;
+
 		std::string filename = getFileName(stage, RAW);
 		FILE* fid = fopen(filename.c_str(), "wb");
 		const uchar* imgPtr;
@@ -73,28 +92,24 @@ public:
 		fclose(fid);
 
 	};
-	inline void saveIntermediate(const ::cv::Mat& intermediate, const int stage) {
+	virtual void saveIntermediate(const ::cv::Mat& intermediate, const int stage) {
+		if (!selected(stage)) return;
+
 		std::string filename = getFileName(stage, suffix);
 		::cv::imwrite(filename, intermediate);
 	};
 
 #if !defined (HAVE_CUDA)
-	inline void imwriteRaw(const ::cv::gpu::GpuMat& intermediate, const int stage) { throw_nogpu(); };
-	inline void saveIntermediate(const ::cv::gpu::GpuMat& intermediate, const int stage) { throw_nogpu(); };
+	virtual void imwriteRaw(const ::cv::gpu::GpuMat& intermediate, const int stage) { throw_nogpu(); };
+	virtual void saveIntermediate(const ::cv::gpu::GpuMat& intermediate, const int stage) { throw_nogpu(); };
 #else
-	inline ::cv::Mat download(const ::cv::gpu::GpuMat& intermediate) {
-		::cv::Mat output(intermediate.size(), intermediate.type());
-		stream.waitForCompletion();
-		stream.enqueueDownload(intermediate, output);
-		stream.waitForCompletion();
-		return output;
-	}
-
-	inline void imwriteRaw(const ::cv::gpu::GpuMat& intermediate, const int stage) {
+	virtual void imwriteRaw(const ::cv::gpu::GpuMat& intermediate, const int stage) {
+		if (!selected(stage)) return;
 		// first download the data
 		imwriteRaw(download(intermediate), stage);
 	};
-	inline void saveIntermediate(const ::cv::gpu::GpuMat& intermediate, const int stage) {
+	virtual void saveIntermediate(const ::cv::gpu::GpuMat& intermediate, const int stage) {
+		if (!selected(stage)) return;
 		// first download the data
 		saveIntermediate(download(intermediate), stage);
 	};
@@ -102,8 +117,22 @@ public:
 #endif
 
 
+};
 
-#endif
+
+class IntermediateResultDiscarder :IntermediateResultHandler {
+
+public:
+	IntermediateResultDiscarder() {};
+
+	virtual ~IntermediateResultDiscarder() {};
+
+	// write out with raw
+	virtual void imwriteRaw(const ::cv::Mat& intermediate, const int stage) {};
+	virtual void saveIntermediate(const ::cv::Mat& intermediate, const int stage) {};
+
+	virtual void imwriteRaw(const ::cv::gpu::GpuMat& intermediate, const int stage) {};
+	virtual void saveIntermediate(const ::cv::gpu::GpuMat& intermediate, const int stage) {};
 
 };
 
