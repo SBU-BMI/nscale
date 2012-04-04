d3d3996f95b5495a6b4a30255142860a8a9a1aca
Tony Pan <tcpan@emory.edu>
2012-04-04 13:29:28 -0400

    ADD: scripts for SC-IO experiments

--- src/studies/SC-IO/experiments/strong-checkpoint-segtest-yellowstone.sh
+++ src/studies/SC-IO/experiments/strong-checkpoint-segtest-yellowstone.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
-DATA_DIR=/home/tcpan/PhD/path/Data
+IN_DATA_DIR=/home/tcpan/PhD/path/Data
+OUT_DATA_DIR=/home/tcpan/PhD/path/Data
 BIN_DIR=/home/tcpan/PhD/path/src/nscale-release/bin
+FILE_COUNT=5
+
 SCRIPT_FILE=$0
 OUT_DIR=${SCRIPT_FILE%.*}
+OUT_PREFIX=$(basename $0 .sh)
 
 echo "running $0"
 
-mkdir -p  -v $OUT_DIR
+mkdir -p -v $OUT_DIR
 
 # strong scaling test using segmentation test data (5 images) with all stages turned on, on yellowstone.
 for i in 2 3 5 9
 do
-	echo "running strong scaling seg test on yellowstone with $i processes and checkpointing all"
-	mpirun -np $i ${BIN_DIR}/nu-segment-scio.exe ${DATA_DIR}/segmentation-tests ${DATA_DIR}/scio-segtest-mpi 5 0-101 cpu 1 > ${OUT_DIR}/scio-segtest-mpi${i}.csv
+	echo "running strong scaling seg test on yellowstone with $((i-1)) worker processes and checkpointing all"
+	mpirun -np $i ${BIN_DIR}/nu-segment-scio.exe ${IN_DATA_DIR}/segmentation-tests ${OUT_DATA_DIR}/scio-segtest-mpi ${FILE_COUNT} 0-101 cpu 1 > ${OUT_DIR}/${OUT_PREFIX}.${i}.csv
 done
 
 echo "$0 completed"
---
+++ src/studies/SC-IO/experiments/weak-checkpoint-validation-ccicluster.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+IN_DATA_DIR=/data/exascale/DATA
+OUT_DATA_DIR=/data/tcpan/output
+BIN_DIR=/data/tcpan/src/nscale-bin/bin
+HOSTFILE=/data/tcpan/hostfiles/gpunodes
+FILE_COUNT=25
+
+SCRIPT_FILE=$0
+OUT_DIR=${SCRIPT_FILE%.*}
+OUT_PREFIX=$(basename $0 .sh)
+
+echo "running $0"
+
+mkdir -p -v $OUT_DIR
+
+# strong scaling test using segmentation test data (5 images) with all stages turned on, on yellowstone.
+for i in 5 9 17 33 65 129
+do
+	echo "running weak scaling with validation data on cci cluster with $((i-1)) worker processes and checkpointing all"
+	TOTAL_COUNT=$((i-1))
+	TOTAL_COUNT=$((TOTAL_COUNT \* FILE_COUNT))
+	mpirun -np $i --hostfile=${HOSTFILE} --bynode ${BIN_DIR}/nu-segment-scio.exe ${DATA_DIR}/ValidationSet ${DATA_DIR}/scio-val-mpi ${TOTAL_COUNT} 0-101 cpu 1 > ${OUT_DIR}/${OUT_PREFIX}.${i}.csv
+done
+
+echo "$0 completed"
