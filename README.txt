41e7bfb0874cf82d2f8451a261e9599ee3aa2c72
Tony Pan <tcpan@emory.edu>
2012-11-06 13:04:29 -0500

    ENH modified syntest.p720.sh for parameter sweep

--- README.txt
+++ README.txt
@@ -1,13 +1,13 @@
+THIS IS NOT UP TO DATE.
+
 Build requirements:
-1. autoconf, automake, libtool 
+1. cmake 
 2. make
 3. a suitable c/c++ compiler
-4. cmake, ccmake
-5. libeigen2-dev, libpng-dev, libpng++-dev, libtiff 3.9.4-5ubuntu6 (through ubuntu)
-
+4. libeigen2-dev, libpng-dev, libpng++-dev, libtiff 3.9.4-5ubuntu6 (through ubuntu)
+5. hdf5 (for pipeline), adios (for SCIO), boost, MPI
 
 This package requires the following external libraries
-Intel TBB 3.0 update 7 oss for lin (through ubuntu)
 CUDA  (need drivers, toolkit, and SDK from NVIDIA)
 OpenCV 2.3.0 (require cmake, and optionally CUDA, TBB).  use system libpnginstead of the built-in. - important.  else get png version issues.  turned off video stuff, on with TBB and CUDA.  turn off QT-opengl. Do make install at the end
 HDF5 4.1.2 shared 64bit for lnux 2.6
--- scripts/keeneland/syntest.p720.sh
+++ scripts/keeneland/syntest.p720.sh
@@ -9,35 +9,51 @@
 ## If left commented, must be specified when the job is submitted:
 ## 'qsub -l walltime=hh:mm:ss,nodes=12:ppn=4'
 ##
-#PBS -l walltime=06:00:00
+#PBS -l walltime=04:00:00
 ##PBS -l nodes=12:ppn=4:gpus=3
-#PBS -l nodes=60:ppn=12
+#PBS -l nodes=50:ppn=12
 ### End of PBS options ###
 
 source /nics/c/home/tcpan/keeneland_env.sh
 
+JOB_PREFIX="synthetic.datasizes.p720"
+
 BINDIR=/nics/c/home/tcpan/builds/nscale-keeneland-cpu
 DATADIR=/lustre/medusa/tcpan/bcrTCGA
 # old, no attention to OSTS.  DATADIR=/lustre/medusa/tcpan/bcrTCGA
-OUTDIR=/lustre/medusa/tcpan/output
+OUTDIR=/lustre/medusa/tcpan/output/${JOB_PREFIX}
 
 cd $OUTDIR
 
-sizes="128 256 512 1024 2048 4096 8192"
+buffer_sizes="2 4 6"
+data_sizes="128 256 512 1024 2048 4096 8192"
+io_sizes="15 30 60 120 240 360 480"
 transports="na-NULL NULL na-POSIX POSIX MPI_AMR"
 
-for transport in ${transports}
+date
+
+for buffer_size in ${buffer_sizes}
 do
-	for size in ${sizes}
+	for data_size in ${data_sizes}
 	do
-		date
-		echo "mpirun --mca mpi_paffinity_alone 1 ${BINDIR}/bin/SynData_Full.exe ${DATADIR} ${OUTDIR}/synthetic.datasizes.p720 18000 cpu ${transport} 4 60 1 15 1 0 ${size}"
-		mpirun --mca mpi_paffinity_alone 1 ${BINDIR}/bin/SynData_Full.exe ${DATADIR} ${OUTDIR}/synthetic.datasizes.p720 18000 cpu ${transport} 4 60 1 15 1 0 ${size} 
-		
-		rm -rf ${OUTDIR}/synthetic.datasizes.p720
-        mv ${OUTDIR}/synthetic.datasizes.p720.csv ${OUTDIR}/synthetic.datasizes.p720.${transport}.${size}.csv
-		
+
+		for io_size in ${io_sizes}
+		do
+			for transport in ${transports}
+			do
+				date
+				echo "mpirun --mca mpi_paffinity_alone 1 ${BINDIR}/bin/SynData_Full.exe ${DATADIR} ${OUTDIR}/syntest.n50.f6000.${transport}.b${buffer_size}.io${io_size}.is15.data${data_size}.b 6000 cpu ${transport} ${buffer_size} ${io_size} 1 15 ${data_size} off off"
+				mpirun --mca mpi_paffinity_alone 1 ${BINDIR}/bin/SynData_Full.exe ${DATADIR} ${OUTDIR}/syntest.n50.f6000.${transport}.b${buffer_size}.io${io_size}.is15.data${data_size}.b 6000 cpu ${transport} ${buffer_size} ${io_size} 1 15 ${data_size} off off
+				rm -rf ${OUTDIR}/syntest.n50.f6000.${transport}.b${buffer_size}.io${io_size}.is15.data${data_size}.b
+
+				echo "mpirun --mca mpi_paffinity_alone 1 ${BINDIR}/bin/SynData_Full.exe ${DATADIR} ${OUTDIR}/syntest.n50.f6000.${transport}.b${buffer_size}.io${io_size}.is15.data${data_size}.nb 6000 cpu ${transport} ${buffer_size} ${io_size} 1 15 ${data_size} off on"
+				mpirun --mca mpi_paffinity_alone 1 ${BINDIR}/bin/SynData_Full.exe ${DATADIR} ${OUTDIR}/syntest.n50.f6000.${transport}.b${buffer_size}.io${io_size}.is15.data${data_size}.nb 6000 cpu ${transport} ${buffer_size} ${io_size} 1 15 ${data_size} off on
+
+				rm -rf ${OUTDIR}/syntest.n50.f6000.${transport}.b${buffer_size}.io${io_size}.is15.data${data_size}.nb
+			done
+		done		
 	done
 done
+date
 
 exit 0
