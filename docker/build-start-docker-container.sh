88594d062ef5024fbf069f03d95b77a5d0d983e9
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2017-04-29 10:09:07 -0400

    Docker
    Add Dockerfile and startup code
    People can use it to get going quickly

---
+++ docker/build-start-docker-container.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#
+# Assumes you have Docker installed on your system, and wish to build
+# your project in a Docker container
+#
+
+echo
+echo "We'll build and start a Docker container, and execute it for you"
+echo "interactively..."
+echo
+
+PROGNAME=$(basename "$0")
+if [[ $# -lt 1 ]] ; then
+	echo "Need project_name parameter. Please start again."
+	echo 'usage: ${PROGNAME} project_name'
+  echo
+else
+	docker build -t $USER/$1 .
+  docker run --name $USER-$1 -it -d $USER/$1 /bin/bash
+  containerId=$(docker inspect --format '{{ .Id }}' $USER-$1)
+  docker exec -it $containerId /bin/bash
+fi
