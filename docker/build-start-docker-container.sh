022d1ebe03abd77c0833ada8e3f6155a5a116219
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2017-04-30 14:19:13 -0400

    update

--- docker/build-start-docker-container.sh
+++ docker/build-start-docker-container.sh
@@ -12,11 +12,14 @@ echo
 
 PROGNAME=$(basename "$0")
 if [[ $# -lt 1 ]] ; then
-	echo "Need project_name parameter. Please start again."
-	echo 'usage: ${PROGNAME} project_name'
+  echo "Need project_name parameter. Please start again."
+  echo 'usage: ${PROGNAME} project_name'
+  echo
+  echo 'example:'
+  echo './build-start-docker-container.sh nscale'
   echo
 else
-	docker build -t $USER/$1 .
+  docker build -t $USER/$1 .
   docker run --name $USER-$1 -it -d $USER/$1 /bin/bash
   containerId=$(docker inspect --format '{{ .Id }}' $USER-$1)
   docker exec -it $containerId /bin/bash
